b7061b24572b06b76148dea400259377863db01f
YanaBrewster <yana.brewster@gmail.com>
2020-02-02 20:02:50 +1300

    Digital clock

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+
 var date = new Date();
 let hr = date.getHours();
 let min = date.getMinutes();
@@ -10,6 +11,7 @@ let sec = date.getSeconds();
 let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 let secPosition = sec * 360 / 60;
+
 
 function runTheClock() {
 
@@ -20,7 +22,42 @@ function runTheClock() {
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+
 }
 
 
 var interval = setInterval(runTheClock, 1000);
+
+
+// DIGITAL CLOCK
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("digitalClock").innerText = time;
+    document.getElementById("digitalClock").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
